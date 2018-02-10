603af36cd8bc5c092c95b9b17a9e5a3840ee40c8
Bryce Thuilot <bthuilot@gmail.com>
2018-02-09 23:25:00 -0500

    Xor working

---
+++ lib/encryption.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+class Encryption {
+
+  String xor(String msg, String otp) {
+    List<int> charValuesTemp = msg.codeUnits;
+    List<int> otpValues = otp.codeUnits;
+    List<int> charValues = new List(otpValues.length);
+
+    for(int j = 0; j < charValuesTemp.length; j++ ) {
+      charValues[j] = charValuesTemp[j];
+    }
+
+
+    if ( charValuesTemp.length < otpValues.length){
+      for( int k = charValuesTemp.length; k < otpValues.length; k++) {
+        charValues[k] = 0;
+      }
+    }
+    List<int> cipher = new List(charValues.length);
+    for(int i = 0; i < charValues.length; i++) {
+      cipher[i] = charValues[i] ^ otpValues[i];
+    }
+
+    return UTF8.decode(cipher);
+  }
+}
+
+main() {
+  Encryption e = new Encryption();
+  print(e.xor('HelloTrevor', '123456789112234567898765432456789'));
+
+}
