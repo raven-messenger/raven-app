d8a100cf17a939ccd6f5c9db47a272d1a0d9fcca
Trevor <stenson124@gmail.com>
2018-02-09 23:48:27 -0500

    added random seed functionality

---
+++ lib/random.dart
@@ -0,0 +1,23 @@
+import 'dart:math';
+import 'encryption.dart';
+
+class RandomSeed {
+  String generateSeed() {
+    var rand = new Random();
+    var codeUnits = new List.generate(30, (index) {
+      return rand.nextInt(100);
+    });
+    return new String.fromCharCodes(codeUnits);
+  }
+}
+
+main() {
+  RandomSeed r = new RandomSeed();
+  Encryption e = new Encryption();
+  String seed = r.generateSeed();
+  print('SEED: ' + seed);
+  String encrypted = e.xor("Hello Trevor", seed);
+  print('ENCRYPTED: ' + encrypted);
+  String decrypted = e.xor(encrypted, seed);
+  print('DECRYPTED: ' + decrypted);
+}
