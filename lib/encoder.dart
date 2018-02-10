2a246e5790f051c33968548f525a6698d73b8369
Trevor <stenson124@gmail.com>
2018-02-10 11:40:40 -0500

    cleaned up encoder functions

--- lib/encoder.dart
+++ lib/encoder.dart
@@ -2,6 +2,8 @@ import 'encryption.dart';
 import 'onetimepads.dart';
 import 'random.dart';
 
+const int REQUIRED_LENGTH = 184;
+
 class Encoder {
   
   Encryption encryption;
@@ -11,13 +13,13 @@ class Encoder {
   }
   
   String encodeMessage(int index, String msg, String otp) {
-    if(msg.length < 184){
-      for(int i = msg.length; i < 184; i++){
+    if(msg.length < REQUIRED_LENGTH){
+      for(int i = msg.length; i < REQUIRED_LENGTH; i++){
         msg += " ";
       }
     }
-    String enc = this.encryption.xor(msg + new DateTime.now().microsecondsSinceEpoch.toString(), otp);
-    return index.toString().padLeft(2, "0") + enc;
+    return index.toString().padLeft(2, "0")
+        + this.encryption.xor(msg + new DateTime.now().microsecondsSinceEpoch.toString(), otp);
   }
 
   String decodeMessage(String msg, String otp) {
@@ -27,7 +29,7 @@ class Encoder {
   DateTime getTime(String msg, String otp) {
     String newmsg = msg.substring(2, msg.length);
     int timecount = int.parse(this.encryption.xor(newmsg, otp).substring
-      (newmsg.length - 16, newmsg.length - 1));
+      (newmsg.length - 16, newmsg.length));
     DateTime time = new DateTime.fromMicrosecondsSinceEpoch(timecount);
     return time;
   }
@@ -39,14 +41,10 @@ main() {
   RandomSeed r = new RandomSeed();
   String seed = r.generateSeed();
   String otp = o.generateOTP(seed);
-
-  String encoded = e.encodeMessage(1, 'hello bryce', otp);
+  String encoded = e.encodeMessage(01, 'hello kyle!', otp);
   print('ENCODED: ' + encoded);
-  print('len: ' + encoded.length.toString());
-  print('len: ' + encoded.toString().substring(2, encoded.length).length.toString());
   DateTime time = e.getTime(encoded, otp);
   print('TIME: ' + time.toString());
   String decoded = e.decodeMessage(encoded, otp);
   print('DECODED: ' + decoded);
-
 }
