64d5d3906ffe2ba7d5125afb8182114125fdce92
Bryce Thuilot <bthuilot@gmail.com>
2018-02-10 16:23:31 -0500

    google sign in works

--- lib/main.dart
+++ lib/main.dart
@@ -212,14 +212,14 @@ class ChatMessage extends StatelessWidget {
     );
   }
 }
-
-class QRScreen extends StatefulWidget {
-  @override
-  State createState() => new QRScreenState();
-}
-
-class QRScreenState extends State<ChatScreen> {
-  Widget build(BuildContext context) {
-    return new Scaffold();
-  }
-}
+//
+//class QRScreen extends StatefulWidget {
+//  @override
+//  State createState() => new QRScreenState();
+//}
+//
+//class QRScreenState extends State<ChatScreen> {
+//  Widget build(BuildContext context) {
+//    return new Scaffold();
+//  }
+//}
