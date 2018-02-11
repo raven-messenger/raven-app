73422b707d16c542cd6468e3d7c6a513495bd0cc
Bryce Thuilot <bthuilot@gmail.com>
2018-02-11 15:08:35 -0500

    Now with 100% less Objective C

--- lib/qrgenscreen.dart
+++ lib/qrgenscreen.dart
@@ -1,10 +1,9 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
-import 'dart:io';
 
 class QRGenScreen extends StatefulWidget {
   @override
@@ -17,8 +16,9 @@ class QRGenScreenState extends State<QRGenScreen> {
   String _fileName = "";
   Image _img = new Image.network("https://static.pexels.com/photos/20787/pexels-photo.jpg");
 
+
   Future<Null> _genQR() async {
-    String fileName;
+    String fileName = "test.png";
     try {
       Map<String, dynamic> args = {
         "fileName": fileName,
@@ -26,13 +26,16 @@ class QRGenScreenState extends State<QRGenScreen> {
       };
       final String result = await platform.invokeMethod('genQR', args);
       fileName = result;
+//      Directory docDir = await getApplicationDocumentsDirectory();
+//      String oldName = fileName;
+//      fileName = docDir.path + "/test1.png";
     } on PlatformException catch (e) {
       fileName = "";
     }
 
     setState(() {
       _fileName = fileName;
-      _img = new Image.file(new File(_fileName));
+      _img = new Image.file(new File(fileName));
     });
   }
 
