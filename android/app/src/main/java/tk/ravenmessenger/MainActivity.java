cc3a469497b4ca9ae87e79204dadfe87319894c6
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2018-02-11 04:38:52 -0500

    early QR code generation

--- android/app/src/main/java/tk/ravenmessenger/MainActivity.java
+++ android/app/src/main/java/tk/ravenmessenger/MainActivity.java
@@ -1,14 +1,59 @@
 package tk.ravenmessenger;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.os.Environment;
 import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+    private static final String CHANNEL = "tk.ravenmessenger/genQR";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
+
+        new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
+                new MethodChannel.MethodCallHandler() {
+                    @Override
+                    public void onMethodCall(MethodCall methodCall, MethodChannel.Result result) {
+                        if (methodCall.method.equals("genQR")) {
+                            String fileName = methodCall.argument("fileName");
+                            String value = methodCall.argument("value");
+                            result.success(genSaveQR(fileName, value));
+                        }
+                    }
+                }
+        );
+    }
+
+    String genSaveQR(String fileName, String value) {
+        Bitmap bmap = QrCode.create(value);
+        String file_path = Environment.getDataDirectory().getAbsolutePath() +
+                "/" + fileName;
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(file_path);
+            bmap.compress(Bitmap.CompressFormat.PNG, 100, out); // bmp is your Bitmap instance
+            // PNG is a lossless format, the compression factor (100) is ignored
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return file_path;
+    }
 }
--- lib/qrgenscreen.dart
+++ lib/qrgenscreen.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-import 'main.dart';
+import 'dart:io';
 
 class QRGenScreen extends StatefulWidget {
   @override
@@ -9,10 +12,40 @@ class QRGenScreen extends StatefulWidget {
 }
 
 class QRGenScreenState extends State<QRGenScreen> {
+  static const platform = const MethodChannel('tk.ravenmessenger/genQR');
+
+  String _fileName = "";
+  Image _img = new Image.network("https://static.pexels.com/photos/20787/pexels-photo.jpg");
+
+  Future<Null> _genQR() async {
+    String fileName;
+    try {
+      Map<String, dynamic> args = {
+        "fileName": fileName,
+        "value": "testqr",
+      };
+      final String result = await platform.invokeMethod('genQR', args);
+      fileName = result;
+    } on PlatformException catch (e) {
+      fileName = "";
+    }
+
+    setState(() {
+      _fileName = fileName;
+      _img = new Image.file(new File(_fileName));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Container(
-      child: new Text("GENERATE QR"),
+      child: new Column(
+        children: <Widget>[
+          new RaisedButton(onPressed: _genQR),
+          new Text(_fileName),
+          _img,
+        ],
+      ),
     );
   }
 }
